9b65b12c4c9b5db4f4145ac151b0ddc712daf15b
Tucker Boniface <Bonifacetucker@gmail.com>
2017-06-27 22:16:17 -0700

    im an idiot

--- main.py
+++ main.py
@@ -127,6 +127,5 @@ def short_url_handler(short_url_request):
 if __name__ == '__main__':
   context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   context.load_cert_chain('/etc/letsencrypt/live/vps2.heyitswither.ml/cert.pem', '/etc/letsencrypt/live/vps2.heyitswither.ml/privkey.pem')
-  /etc/letsencrypt/live/vps2.heyitswither.ml
   urls_file_exists()
   app.run(debug=True, host='0.0.0.0', port=8080, ssl_context=context)
